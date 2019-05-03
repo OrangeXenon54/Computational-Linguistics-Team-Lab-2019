f6f78aaaee00f063b5f0c55bbc8725259e94844c
OrangeXenon54 <49636466+OrangeXenon54@users.noreply.github.com>
2019-05-03 10:45:31 +0200

    Added Features
    I added some features and cleaned some things up:
    1.) Got rid of the redundant line breaks in the printing iterations.
    2.) Because the total number of types of POS tags was off by one, given the counts, I made a sorted list to print out all POS tags independent of their count, which added a quote mark that isn't counted for some reason.
    3.) Added a new list that stores the first instance of a POS tag as an example.

--- POSTagTotaler.py
+++ POSTagTotaler.py
@@ -1,30 +1,42 @@
-import re
-
 filename = input("Please type file name:")
 
 devtext = open(filename)
 
 allpos = []
 seen = set()
+TagEx = []
 
+#Iterate through the text, making a list of every POS tag in it
+#Also find the first example of each POS instance and count the POS as seen
 for line in devtext :
     if line == "\n" :
         continue
     else :
         stripped = line.strip()
-        # WTF am I thinking? postag = re.search('\s(.*)$', stripped)
         postag = stripped.split()[1]
         allpos.append(postag)
         if postag not in seen :
             seen.add(postag)
+            POSex = stripped.split()[0]
+            TagEx.append([postag, POSex])
         else :
             continue
 
-sortpos = sorted(allpos)
+#Sort the lists of all tags and examples and total number of POS seen
+allpos = sorted(allpos)
+TagEx = sorted(TagEx)
+seencount = len(seen)
+
+#Convert seen, an unordered set, to a sorted list
+sortseen = []
+for item in seen :
+    sortseen.append(item)
+sortseen = sorted(sortseen)
+
+#Count the total instances of a POS and make a list of lists
 totals = []
 countword = None
-
-for pos in sortpos :
+for pos in allpos :
     if countword is None :
         countword = pos
         postotal = [countword, 1]
@@ -35,6 +47,18 @@ for pos in sortpos :
         countword = pos
         postotal = [countword, 1]
 
-print("Total types of POS tags in data: ", len(seen), "\n")
+
+#Print total number of tags and all tags found
+print("Total types of POS tags in data: ", seencount, "\n")
+print("List of all tags: \n")
+for item in sortseen :
+    print(item)
+
+print("----------------------------")
+print("POS tag total counts: \n")
 for total in totals:
-    print(total, "\n")
+    print(total)
+print("----------------------------")
+print("POS tag examples: \n")
+for item in TagEx:
+    print(item[0], ": ", item[1])
