ba397a0a3597fc7a8df90de495f3218b11838ac9
OrangeXenon54 <49636466+OrangeXenon54@users.noreply.github.com>
2019-05-16 16:23:28 +0200

    Feature Checker Update
    Updated the FeatureChecker function to return data in the agreed upon way (a list of 0s or 1s in the agreed upon order of features)

--- FeatureChecker.py
+++ FeatureChecker.py
@@ -1,5 +1,21 @@
 import re
 
+#Returns a list of lists where each list contains 2 items: the token and its tag
+#Argument is a text file that breaks down into a list of strings
+def TokPOSList(datafile) :
+    TPList = []
+    for line in datafile :
+        if line == "\n" :
+            TPList.append(word)
+            continue
+        else :
+            stripped = line.strip()
+            word = re.findall('(^\S*)\s', stripped)[0]
+            postag = re.findall('\s(\S*$)', stripped)[0]
+            tp = [word, postag]
+            TPList.append(tp)
+    return TPList
+
 #Returns a list of strings from the data with only the tokens and new lines so that tokens can be analyzed and POS tags added later
 #Argument is a text file that breaks down into a list of strings
 def NoPOSList(datafile) :
@@ -40,58 +56,110 @@ def SenList(tokenlist) :
             sentence.append(word)
     return allsentences
 
+featurenameorder = ["BIAS", "eED", "eIED", "eS", "eES", "eIES", "eLY", "eING", "eINGS", "bUN", "bNON", "eFUL", "eER", "eIER", "eEST", "eIEST", "bCAP", "iNUM", "bAPOS"]
+
 #Returns a list of lists with the list consisting of the token -a string -and its features -a set
 #For now, only assigns features on single token basis not in context
 #Argument is a list of strings
-def FeatureChecker(tokenlist) :
-    tokANDfeat = []
-    for word in tokenlist:
-        if word == "\n" :
-            tokANDfeat.append(word)
+def FeatureChecker(token) :
+    features = []
+    if token == "\n" :
+        features.append("NULL")
+        #features = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    else :
+        #BIAS
+        features.append(1.)
+        #eED
+        if re.search('\S+ed$', token) :
+            features.append(1.)
+        else:
+            features.append(0.)
+        #eIED
+        if re.search('\S+ied', word) :
+            features.append(1.)
         else :
-            feats = set()
-            if re.search('\S+ed$', word) :
-                if re.search('\S+ied', word) :
-                    feats.add("featIED")
-                else :
-                    feats.add("featED")
-            if re.search('\S+er$', word) :
-                if re.search('\S+ier$', word) :
-                    feats.add("featIER")
-                else :
-                    feats.add("featER")
-            if re.search('\S+s$', word) :
-                if re.search('\S+ies$', word) :
-                    feats.add("featIES")
-                elif re.search('\S+es$', word) :
-                    feats.add("featES")
-                else :
-                    feats.add("featS")
-            if re.search('^[0-9]+', word) :
-                feats.add("featNUM")
-            if re.search('\S+ing$', word) or re.search('\S+ings$', word) :
-                feats.add("featING")
-            if re.search('\S+ly$', word) :
-                feats.add("featLY")
-            if re.search('\S+ful$', word) :
-                feats.add("featFUL")
-            if re.search('^[Uu]n\S+', word) or re.search('(^[Nn]on)\S+', word) :
-                feats.add("featNEGP")
-            if re.search('^[A-Z]\S+', word) :
-                feats.add("featCAP")
-            if re.search('\S+est$', word) :
-                if re.search('\S+iest$', word) :
-                    feats.add("featIEST")
-                else :
-                    feats.add("featEST")
-            if re.search('^\'\S+', word) :
-                feats.add("featAPOS")
-            if len(feats) <= 0 :
-                tokfeats = [word]
-            else :
-                tokfeats = [word, feats]
-            tokANDfeat.append(tokfeats)
-    return tokANDfeat
+            features.append(0.)
+        #eS
+        if re.search('\S+s$', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #eES
+        if re.search('\S+es$', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #eIES
+        if re.search('\S+ies$', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #eLY
+        if re.search('\S+ly$', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #eING
+        if re.search('\S+ing$', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #eINGS
+        if re.search('\S+ings$', word):
+            features.append(1.)
+        else :
+            features.append(0.)
+        #bUN
+        if re.search('^[Uu]n\S+', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #bNON
+        if re.search('(^[Nn]on)\S+', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #eFUL
+        if re.search('\S+ful$', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #eER
+        if re.search('\S+er$', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #eIER
+        if re.search('\S+ier$', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #eEST
+        if re.search('\S+est$', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #eIEST
+        if re.search('\S+iest$', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #bCAP
+        if re.search('^[A-Z]\S+', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iNUM
+        if re.search('^[0-9]+', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #bAPOS
+        if re.search('^\'\S+', word) :
+            features.append(1.)
+        else :
+            features.append(0.)
+    return features
 
 filename = input("Please type file name:")
 devtext = open(filename)
@@ -104,36 +172,26 @@ tokenfeatures = FeatureChecker(justtokens)
 print("***All features added to tokens***\n")
 
 """
-A.)	featED
-1.)	the word ends in “ed”
-B.)	featIED
-1.)	the word ends in “ied”
-C.)	featS
-1.)	the word ends in “s”
-D.)	featES
-1.)	the word ends in “es”
-E.)	featIES
-1.)	the word ends in “ies”
-F.)	featLY
-1.)	the word ends in “ly”
-G.)	featING
-1.)	the word ends in “ing”
-H.)	featNEGP
-1.)	the word begins with “un” or “non”
-I.)	featFUL
-1.)	the word ends in “ful”
-J.)	featER
-1.)	the word ends in “er”
-K.)	featIER
-1.)	the word ends in “ier”
-L.)	featEST
-1.)	the word ends in “est”
-M.)	featIEST
-1.)	the word ends in “iest”
-N.)	featCAP
-1.)	the word begins with a capital letter and is not the first word in the sentence
-O.)	featNUM
-1.)	The word contains a numeric character
-P.) featAPOS
-1.) The word starts with an apostrophe
+1.)	BIAS: just something you add to the feature vector
+2.)	eED: the word ends in “ed”
+3.)	eIED: the word ends in “ied”
+4.)	eS: the word ends in “s”
+5.)	eES: the word ends in “es”
+6.)	eIES: the word ends in “ies”
+7.)	eLY: the word ends in “ly”
+8.)	eING: the word ends in “ing”
+9.)	eINGS: the word ends in "ings"
+10.) bUN: the word begins with "un"
+11.) bNON: the word begins with "non"
+12.) eFUL: the word ends in “ful”
+13.) eER: the word ends in “er”
+14.) eIER: the word ends in “ier”
+15.) eEST: the word ends in “est”
+16.) eIEST: the word ends in “iest”
+17.) bCAP: the word begins with a capital letter and is not the first word in the sentence
+18.) iNUM: The word contains a numeric character
+19.) bAPOS: The word starts with an apostrophe
+e = ends with
+b = begins with
+i = is
 """
