8b3b66392f8b9feb4fe31920fa291c47a2e86687
Adnan Ahmad <adnan.sust@outlook.com>
2019-05-20 20:04:49 +0200

    Add files via upload

---
+++ Naive Bayes final/ClassifierTrain.py
@@ -0,0 +1,226 @@
+import csv
+import json
+
+
+TRAINING_PATH = "model with dev data as training/test.col"
+
+words = {}
+original_label =  {}
+predicted_label =  {}
+tagset = set([])
+tag_count = {}
+overall_counts = {}
+
+
+print("Loading training data...")
+# load the training data
+with open(TRAINING_PATH) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter='\t')
+    i = 0
+    for row in csv_reader:
+        if len(row) != 0:
+            original_label[i] = row[1]
+            words[i] = row[0]
+            tagset.add(row[1])
+        else:
+            original_label[i] = 'STT'
+            words[i] = '<S>'
+        i +=1
+
+print(len(words))
+print(len(tagset))
+
+def save_tag_count(current_word_index):
+    print("Saving tag count...")
+    tag_count = {}
+    for i in range(current_word_index):
+        if original_label[i] in tag_count.keys():
+            tag_count[original_label[i]] = tag_count[original_label[i]] + 1
+        else:
+            tag_count[original_label[i]] = 1
+
+    with open(str(current_word_index)+' tag_count.txt', 'w') as tag_count_file:
+        tag_count_file.write(json.dumps(tag_count))
+        print(len(tag_count))
+        print('done')
+
+
+def save_model_count(dict_pos, current_word_index):
+    print("Saving model...")
+    with open(str(current_word_index)+' model.txt', 'w') as file:
+        file.write(json.dumps(dict_pos))
+        print(len(dict_pos))
+        print(str(current_word_index) +' done')
+
+# method to get the list of features of current word
+def get_feature_list(i):
+
+    global overall_counts
+    global words
+    f_index = 0
+    f = {}
+
+    try:
+        f[f_index] = words[i]
+    except:
+        f[f_index] = 'nil'
+
+    f_index += 1
+
+    try:
+        f[f_index] = words[i] + words[i - 1]
+    except:
+        f[f_index] = 'nil'
+    f_index += 1
+
+    try:
+        f[f_index] = words[i] + words[i - 2]
+    except:
+        f[f_index] = 'nil'
+    f_index += 1
+
+    try:
+        f[f_index] = words[i] + words[i - 1] + words[i - 2]
+    except:
+        f[f_index] = 'nil'
+    f_index += 1
+
+    try:
+        f[f_index] = words[i] + words[i + 1]
+    except:
+        f[f_index] = 'nil'
+    f_index += 1
+
+    try:
+        f[f_index] = words[i] + words[i + 2]
+    except:
+        f[f_index] = 'nil'
+    f_index += 1
+
+    try:
+        f[f_index] = words[i] + words[i + 1] + words[i + 2]
+    except:
+        f[f_index] = 'nil'
+    f_index += 1
+
+    try:
+        f[f_index] = words[i + 1]
+    except:
+        f[f_index] = 'nil'
+    f_index += 1
+
+    try:
+        f[f_index] = words[i + 1] + words[i + 2]
+    except:
+        f[f_index] = 'nil'
+    f_index += 1
+
+    try:
+        f[f_index] = words[i - 1]
+    except:
+        f[f_index] = 'nil'
+    f_index += 1
+
+    try:
+        f[f_index] = words[i - 1] + words[i - 2]
+    except:
+        f[f_index] = 'nil'
+    f_index += 1
+
+    try:
+        f[f_index] = words[i - 1] + words[i + 1]
+    except:
+        f[f_index] = 'nil'
+    f_index += 1
+
+    try:
+        f[f_index] = words[i - 2] + words[i - 1] + words[i + 1] + words[i + 2]
+    except:
+        f[f_index] = 'nil'
+    f_index += 1
+
+    for i in range(len(f)):
+        if f[i] in overall_counts.keys():
+            count = overall_counts[f[i]]
+            overall_counts[f[i]] = count + 1
+        else:
+            overall_counts[f[i]] = 1
+
+    return f
+
+
+"""""
+# count of feature given pos for every pos tag
+
+dict_pos[pos_tag] = {} # 48 pos tags
+dict_feature_given_pos[f_index] = {} # 13 features for each pos tag
+dict_feature_count[feature_word] = {} # count of each features(ex. current word, next word) for each feature of each tag
+
+"""""
+
+dict_pos = {}
+overall_counts = {}
+
+
+# for each word
+# get the features
+# update the dictionary of each POS > feature_index=feature_token=token_count mapping
+def get_count_of_features_per_pos():
+
+    global dict_pos
+
+    for i in range(len(words)):
+
+        if(i % 1000) == 0:
+            print("Saving the model at index " + str(i))
+            save_model_count(dict_pos, i)
+            save_tag_count(i)
+
+        #       print(words[i])
+        #       print(original_label[i])
+        #       print("\n")
+
+        f = get_feature_list(i)
+
+        if original_label[i] in dict_pos.keys():
+
+            temp_dict_feature_given_pos = dict_pos[original_label[i]]
+            for f_index in range(len(temp_dict_feature_given_pos)):
+
+                temp_feature_word_count_dict = temp_dict_feature_given_pos[f_index]
+
+                if f[f_index] != 'nil':
+
+                    if f[f_index] in temp_feature_word_count_dict.keys():
+
+                        temp_feature_word_count = temp_feature_word_count_dict[f[f_index]]
+                        temp_feature_word_count_dict[f[f_index]] = temp_feature_word_count + 1
+                    else:
+
+                        temp_feature_word_count_dict[f[f_index]] = 1
+
+                temp_dict_feature_given_pos[f_index] = temp_feature_word_count_dict
+            dict_pos[original_label[i]] = temp_dict_feature_given_pos
+
+        else:
+
+            temp_dict_feature_given_pos = {}
+            temp_feature_word_count_dict = {}
+            for f_index in range(len(f)):
+
+                if f[f_index] != 'nil':
+
+                    temp_feature_word_count_dict[f[f_index]] = 1
+
+                temp_dict_feature_given_pos[f_index] = temp_feature_word_count_dict
+            dict_pos[original_label[i]] = temp_dict_feature_given_pos
+
+
+get_count_of_features_per_pos()
+
+
+
+
+
+
+
---
+++ Naive Bayes final/Evaluation.py
@@ -0,0 +1,124 @@
+import csv
+
+
+ORIGINAL_LABEL_PATH = "model with dev data as training/dev.col"
+PREDICTED_LABEL_PATH = "dev-predict.col"
+
+words = {}
+original_label =  {}
+predicted_label =  {}
+tagset = set([])
+
+with open(ORIGINAL_LABEL_PATH) as csv_file: # file with original label
+    csv_reader = csv.reader(csv_file, delimiter='\t')
+    line_count = 0
+    for row in csv_reader:
+        if len(row) != 0:
+            original_label[line_count] = row[1]
+            words[line_count] = row[0]
+            tagset.add(row[1])
+        else:
+            original_label[line_count] = 'STT'
+            words[line_count] = '<S>'
+        line_count +=1
+
+with open(PREDICTED_LABEL_PATH) as csv_file:  # file with predicted label
+    csv_reader = csv.reader(csv_file, delimiter='\t')
+    line_count = 0
+    for row in csv_reader:
+        if len(row) != 0:
+            predicted_label[line_count] = row[1]
+        else:
+            predicted_label[line_count] = 'STT'
+        line_count += 1
+
+print(len(words))
+print(len(original_label))
+print(len(predicted_label))
+print(len(tagset))
+
+# A macro-average will compute the metric independently for each class and then take the average (hence treating all classes equally), whereas a micro-average will aggregate the contributions of all classes to compute the average metric.
+# In a multi-class classification setup, micro-average is preferable if you suspect there might be class imbalance
+
+
+
+micro_tp = 0
+micro_fn = 0
+micro_fp = 0
+
+def fscore_function(pos):
+    global micro_tp
+    global micro_fn
+    global micro_fp
+
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+
+    precision = 0
+    recall = 0
+    fscore = 0
+
+    for i in words:
+      if original_label[i] == pos:
+          if predicted_label[i] == pos:
+              tp+= 1
+          else:
+              fn+= 1
+
+      else:
+          if predicted_label[i] == pos:
+              fp+= 1
+          else:
+              tn+=1
+
+    #print(str(tp) + " " + str(tn) + " " + str(fp) + " " + str(fn))
+
+    micro_tp+= tp
+    micro_fn+= fn
+    micro_fp+= fp
+
+    if tp+fp != 0:
+        precision = float(tp/float(tp+fp))
+    else:
+        precision = 1010101
+        #print('precission cannot be calculated, tp+fp = 0')
+
+    if tp+fn != 0:
+        recall = float(tp/float(tp+fn))
+    else:
+        precision = 1010101
+        #print('recall cannot be calculated,  tp+fn = 0')
+
+    if precision+recall == 0:
+        fscore = 0
+    else:
+        fscore = 2*float(float(recall * precision) / float(recall + precision))
+
+    print pos +","+ str("{0:.3f}".format(precision))+","+ str("{0:.3f}".format(recall))+","+ str("{0:.3f}".format(fscore))
+
+for tag in tagset:
+    if len(tag) > 0:
+        fscore_function(tag)
+
+
+print('Micro avg')
+def micro_function(tp, fn, fp):
+    print(str(tp) + " " + str(fp) + " " + str(fn))
+    precision = float(tp/float(tp+fp))
+    recall = float(tp/float(tp+fn))
+
+    print('P : ' + str(precision))
+    print('R : ' + str(recall))
+
+    fscore = 0
+
+    if precision + recall == 0:
+        fscore = 0
+    else:
+        fscore = 2 * float(float(recall * precision) / float(recall + precision))
+        print('F :' + str(fscore))
+
+
+micro_function(micro_tp, micro_fn, micro_fp)
