cc70e56bfe29edadcac89d36d1d8f0752bb460a6
OrangeXenon54 <49636466+OrangeXenon54@users.noreply.github.com>
2019-05-20 12:19:22 +0200

    Specific Word Checking Added
    Added the checking of specific words, particularly the 25 most used prepositions, determiners, pronouns, wh-adverbs, basic be verbs, etc. We might have to delete this in case they ask us to change how we do the Perceptron.

--- BLFeatures.py
+++ BLFeatures.py
@@ -13,8 +13,6 @@ def NoPOSList(datafile) :
             tokensNoPOS.append(word)
     return tokensNoPOS
 
-featurenameorder = ["BIAS", "eED", "eIED", "eS", "eES", "eIES", "eLY", "eING", "eINGS", "bUN", "bNON", "eFUL", "eER", "eIER", "eEST", "eIEST", "bCAP", "iNUM", "bAPOS"]
-
 #Returns a list of lists with the list consisting of the token -a string -and its features -a set
 #For now, only assigns features on single token basis not in context
 #Argument is a list of strings
@@ -181,6 +179,296 @@ def FeatureChecker(token) :
             features.append(1.)
         else :
             features.append(0.)
+        #iOF
+        if token is "of" or "OF" or "Of" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iIN
+        if token is "in" or "IN" or "In" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iTO
+        if token is "to" or "TO" or "To" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iFOR
+        if token is "FOR" or "for" or "For" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iWITH
+        if token is "WITH" or "with" or "With" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iON
+        if token is "ON" or "on" or "On" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iAT
+        if token is "at" or "AT" or "At" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iFROM
+        if token is "from" or "FROM" or "From" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iBY
+        if token is "BY" or "by" or "By" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iABOUT
+        if token is "ABOUT" or "about" or "About" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iAS
+        if token is "as" or "AS" or "As" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iINTO
+        if token is "INTO" or "into" or "Into" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iLIKE
+        if token is "like" or "LIKE" or "Like" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iTHROUGH
+        if token is "through" or "THROUGH" or "Through" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iAFTER
+        if token is "AFTER" or "after" or "After" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iOVER
+        if token is "OVER" or "Over" or "over" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iBETWEEN
+        if token is "BETWEEN" or "between" or "Between" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iOUT
+        if token is "OUT" or "out" or "Out" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iAGAINST
+        if token is "AGAINST" or "against" or "Against" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iDURING
+        if token is "DURING" or "during" or "During" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iWITHOUT
+        if token is "WITHOUT" or "without" or "Without" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iBEFORE
+        if token is "BEFORE" or "Before" or "before" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iUNDER
+        if token is "UNDER" or "under" or "Under" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iAROUND
+        if token is "AROUND" or "around" or "Around" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iAMONG
+        if token is "AMONG" or "among" or "Among" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iTHE
+        if token is "the" or "THE" or "The" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iA
+        if token is "a" or "A" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iAN
+        if token is "an" or "AN" or "An" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iI
+        if token is "I" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iYOU
+        if toekn is "YOU" or "you" or "You" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iTHEY
+        if token is "THEY" or "they" or "They" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iHE
+        if token is "he" or "HE" or "He" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iSHE
+        if token is "she" or "SHE" or "She" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iIT
+        if token is "it" or "IT" or "It" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iIS
+        if token is "is" or "IS" or "Is" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iWAS
+        if token is "was" or "WAS" or "Was" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iARE
+        if token is "are" or "ARE" or "Are" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iAM
+        if token is "am" or "AM" or "Am" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iWERE
+        if token is "WERE" or "Were" or "were" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iWHO
+        if token is "WHO" or "who" or "Who" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iWHAT
+        if token is "WHAT" or "What" or "what" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iWHEN
+        if token is "WHEN" or "When" or "when" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iTHEN
+        if token is "then" or "THEN" or "Then" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iWHERE
+        if token is "where" or "WHERE" or "Where" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iTHERE
+        if token is "THERE" or "There" or "there" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iWHY
+        if token is "WHY" or "why" or "Why" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iHOW
+        if token is "HOW" or "How" or "how" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iPERIOD
+        if token is "." :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iQUESTION
+        if token is "?" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iCOMMA
+        if token is "," :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iCOLON
+        if token is ":" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iSEMICOLON
+        if token is ";" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iLRB
+        if token is "(" or "{" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iRRB
+        if token is ")" or "}" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iLQUOTE
+        if token is "``" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iRQUOTE
+        if token is "''" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iDOLLAR
+        if token is "$" :
+            features.append(1.)
+        else :
+            features.append(0.)
+        #iHYPH
+        if token is "-" :
+            features.append(1.)
+        else :
+            features.append(0.)
     return features
 
 filename = input("Please type file name:")
@@ -230,6 +518,7 @@ for item in toksANDfeats :
 31.) eSIONS: ends in "sions"
 32.) eMENT: ends in "ment"
 33.) eMENTS: ends in "ments"
++ more specific word checking features for common words
 e = ends with
 b = begins with
 i = is
